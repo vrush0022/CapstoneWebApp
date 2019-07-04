40c767ba04c60eb95217750e7825f235f51b3a05
Vrushank Gude <vrush0022@gmail.com>
2019-07-04 20:04:27 +0530

    added exif

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from PIL import Image,ImageDraw
 from tensorflow.keras.backend import set_session
 import tensorflow as tf
 from tensorflow.keras.models import model_from_json
-
+from PIL import ExifTags
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,13 +25,13 @@ app.config['PREDICTIONS_FOLDER'] = PREDICTIONS_FOLDER
 graph = tf.get_default_graph()
 sess = tf.Session(graph=graph)
 set_session(sess)
-json_file = open('model/model2_arch.json', 'r')
+json_file = open('model/model3_arch.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 #load architecture
 model = model_from_json(loaded_model_json)
 # load weights into model
-model.load_weights("model/model2.h5")
+model.load_weights("model/model3.h5")
 
 
 @app.route("/")
@@ -58,19 +58,24 @@ def showResult():
         key=request.form['key']
         uploadedimage=Image.open(os.path.join(app.config['UPLOAD_FOLDER'],key))
         uploadedimage=uploadedimage.resize(size=(256,256))
-        scaledimage=np.array(uploadedimage)/255.
-        pred=predict(scaledimage)
-        if(np.count_nonzero(pred.flatten())>100):#atleast 100 pixels should be classified as fake
-            
-            indices = np.where(pred[0,:,:,0] == 1)
-            upper = np.min(indices[0])
-            lower = np.max(indices[0])
-            left = np.min(indices[1])
-            right = np.max(indices[1])
-            
-            draw = ImageDraw.Draw(uploadedimage)
-            draw.rectangle([(left,upper),(right,lower)],outline=(0,255,0),width=3)
-            msg='Image Classified as Fake'
+        verifyExif=checkExifData(uploadedimage)
+        if verifyExif==False:
+            uploadedimage=uploadedimage.resize(size=(256,256))
+            scaledimage=np.array(uploadedimage)/255.
+            pred=predict(scaledimage)
+            if(np.count_nonzero(pred.flatten())>100):#atleast 100 pixels should be classified as fake
+                
+                indices = np.where(pred[0,:,:,0] == 1)
+                upper = np.min(indices[0])
+                lower = np.max(indices[0])
+                left = np.min(indices[1])
+                right = np.max(indices[1])
+                
+                draw = ImageDraw.Draw(uploadedimage)
+                draw.rectangle([(left,upper),(right,lower)],outline=(0,255,0),width=3)
+                msg='Image Classified as Fake'
+            else:
+                msg='Image Classified as Pristine'
         else:
             msg='Image Classified as Pristine'
             
@@ -99,3 +104,17 @@ def predict(img):
         pred=model.predict(img.reshape(1,256,256,3))
     pred= (pred > 0.5).astype(np.uint8)    
     return pred
+
+
+def checkExifData(img):
+    toReturn=False
+    if img.format=='JPEG':
+        exifDataRaw = img._getexif()
+        if exifDataRaw!=None:
+            exifData = {}
+            for tag, value in exifDataRaw.items():
+                decodedTag = ExifTags.TAGS.get(tag, tag)
+                exifData[decodedTag] = value
+            if exifData.get('Software')==None:
+                toReturn=True
+    return toReturn
